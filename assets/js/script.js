20021e0f87c5c9a685f592044ce22316168aa517
richseazon <106032434+richseazon@users.noreply.github.com>
2023-11-22 19:38:55 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+new Freezeframe('.freezeframe1', {
+  trigger: 'hover',
+  overlay: false
+});
+
+new Freezeframe('.freezeframe2', {
+  trigger: 'click',
+  overlay: true
+});
+
+new Freezeframe('.freezeframe3', {
+  trigger: 'hover',
+  overlay: true
+});
